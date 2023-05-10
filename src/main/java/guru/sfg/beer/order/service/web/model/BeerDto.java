0d290ce88626270d4561353467429fc1bbb39aad
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-10 03:25:43 +0200

    Enhance Order Response with Beer

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,38 @@
+package guru.sfg.beer.order.service.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto implements Serializable {
+
+    private UUID id = null;
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate = null;
+
+    @JsonProperty("name")
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING)
+    private BigDecimal price;
+}
--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
@@ -22,6 +22,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -40,8 +41,11 @@ public class BeerOrderLineDto extends BaseItem {
         this.orderQuantity = orderQuantity;
     }
 
+
     private String upc;
     private String beerName;
+    private String beerStyle;
     private UUID beerId;
     private Integer orderQuantity = 0;
+    private BigDecimal price;
 }
