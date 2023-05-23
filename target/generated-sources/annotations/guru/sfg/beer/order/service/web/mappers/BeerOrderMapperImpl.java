c79241c02c47d090b8599a355f6d7a59e90288d9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-24 01:38:23 +0200

    adding update artemis config

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-19T23:33:10+0200",
+    date = "2023-05-24T01:24:15+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
