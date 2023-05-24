3238779e55d4540f2f2d5a41498e1d4eea0b3795
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-25 01:46:52 +0200

    adding state machine enumerations

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-24T01:24:15+0200",
+    date = "2023-05-25T01:09:22+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -3,6 +3,7 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.domain.BeerOrder.BeerOrderBuilder;
 import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
 import guru.sfg.beer.order.service.domain.Customer;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto.BeerOrderDtoBuilder;
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-24T01:24:15+0200",
+    date = "2023-05-25T01:09:22+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
@@ -103,21 +104,21 @@ public class BeerOrderMapperImpl implements BeerOrderMapper {
         return list;
     }
 
-    protected OrderStatusEnum orderStatusEnumToOrderStatusEnum(guru.sfg.beer.order.service.domain.OrderStatusEnum orderStatusEnum) {
-        if ( orderStatusEnum == null ) {
+    protected OrderStatusEnum orderStatusEnumToOrderStatusEnum(BeerOrderStatusEnum beerOrderStatusEnum) {
+        if ( beerOrderStatusEnum == null ) {
             return null;
         }
 
         OrderStatusEnum orderStatusEnum1;
 
-        switch ( orderStatusEnum ) {
+        switch (beerOrderStatusEnum) {
             case NEW: orderStatusEnum1 = OrderStatusEnum.NEW;
             break;
             case READY: orderStatusEnum1 = OrderStatusEnum.READY;
             break;
             case PICKED_UP: orderStatusEnum1 = OrderStatusEnum.PICKED_UP;
             break;
-            default: throw new IllegalArgumentException( "Unexpected enum constant: " + orderStatusEnum );
+            default: throw new IllegalArgumentException( "Unexpected enum constant: " + beerOrderStatusEnum);
         }
 
         return orderStatusEnum1;
@@ -136,23 +137,23 @@ public class BeerOrderMapperImpl implements BeerOrderMapper {
         return set;
     }
 
-    protected guru.sfg.beer.order.service.domain.OrderStatusEnum orderStatusEnumToOrderStatusEnum1(OrderStatusEnum orderStatusEnum) {
+    protected BeerOrderStatusEnum orderStatusEnumToOrderStatusEnum1(OrderStatusEnum orderStatusEnum) {
         if ( orderStatusEnum == null ) {
             return null;
         }
 
-        guru.sfg.beer.order.service.domain.OrderStatusEnum orderStatusEnum1;
+        BeerOrderStatusEnum beerOrderStatusEnum1;
 
         switch ( orderStatusEnum ) {
-            case NEW: orderStatusEnum1 = guru.sfg.beer.order.service.domain.OrderStatusEnum.NEW;
+            case NEW: beerOrderStatusEnum1 = BeerOrderStatusEnum.NEW;
             break;
-            case READY: orderStatusEnum1 = guru.sfg.beer.order.service.domain.OrderStatusEnum.READY;
+            case READY: beerOrderStatusEnum1 = BeerOrderStatusEnum.READY;
             break;
-            case PICKED_UP: orderStatusEnum1 = guru.sfg.beer.order.service.domain.OrderStatusEnum.PICKED_UP;
+            case PICKED_UP: beerOrderStatusEnum1 = BeerOrderStatusEnum.PICKED_UP;
             break;
             default: throw new IllegalArgumentException( "Unexpected enum constant: " + orderStatusEnum );
         }
 
-        return orderStatusEnum1;
+        return beerOrderStatusEnum1;
     }
 }
