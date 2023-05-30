0c163d27a2f4573e1c003fb4a0a86749af57c604
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-30 02:14:55 +0200

    Using Awaitility

--- src/test/java/guru/sfg/beer/order/service/BeerOrderManagerImplIT.java
+++ src/test/java/guru/sfg/beer/order/service/BeerOrderManagerImplIT.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -83,7 +84,11 @@ public class BeerOrderManagerImplIT {
 
         BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
 
-        Thread.sleep(5000);
+        await().untilAsserted(()->{
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).orElseThrow(IllegalArgumentException::new);
+            assertEquals(BeerOrderStatusEnum.ALLOCATION_PENDING, foundOrder.getOrderStatus());
+        });
+
 
         savedBeerOrder=beerOrderRepository.findById(savedBeerOrder.getId()).orElseThrow(IllegalArgumentException::new);
 
