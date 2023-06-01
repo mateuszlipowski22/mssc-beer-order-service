714d6dac9e10d564c89fbb3320e2b63b9d913f04
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-06-02 00:28:10 +0200

    adding Failed Validation Compensating Transaction

---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/ValidationFailureAction.java
@@ -0,0 +1,23 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidationFailureAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
+        String beerOrderId = (String) stateContext.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        log.error("Compensation transaction.... Validation Failed: "+beerOrderId);
+
+    }
+}
