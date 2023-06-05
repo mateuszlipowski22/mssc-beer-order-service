95278c98ff9e84e9c3f989b7b4127c9f8abb96f7
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-06-06 01:35:39 +0200

    setting port 8081

---
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/CustomerMapperImpl.java
@@ -0,0 +1,59 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.domain.Customer.CustomerBuilder;
+import guru.sfg.brewery.model.CustomerDto;
+import guru.sfg.brewery.model.CustomerDto.CustomerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-06T01:09:12+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
+)
+@Component
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public CustomerDto customerToDto(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDtoBuilder customerDto = CustomerDto.builder();
+
+        customerDto.id( customer.getId() );
+        if ( customer.getVersion() != null ) {
+            customerDto.version( customer.getVersion().intValue() );
+        }
+        customerDto.createdDate( dateMapper.asOffsetDateTime( customer.getCreatedDate() ) );
+        customerDto.lastModifiedDate( dateMapper.asOffsetDateTime( customer.getLastModifiedDate() ) );
+        customerDto.customerName( customer.getCustomerName() );
+
+        return customerDto.build();
+    }
+
+    @Override
+    public Customer dtoToCustomer(CustomerDto customerDto) {
+        if ( customerDto == null ) {
+            return null;
+        }
+
+        CustomerBuilder customer = Customer.builder();
+
+        customer.id( customerDto.getId() );
+        if ( customerDto.getVersion() != null ) {
+            customer.version( customerDto.getVersion().longValue() );
+        }
+        customer.createdDate( dateMapper.asTimestamp( customerDto.getCreatedDate() ) );
+        customer.lastModifiedDate( dateMapper.asTimestamp( customerDto.getLastModifiedDate() ) );
+        customer.customerName( customerDto.getCustomerName() );
+
+        return customer.build();
+    }
+}
