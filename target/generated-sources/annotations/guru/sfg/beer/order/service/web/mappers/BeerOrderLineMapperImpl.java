3e85a608f44b30431251989cceae8764578669cc
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-06-07 02:26:20 +0200

    adding eureka config

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-03T21:29:20+0200",
+    date = "2023-06-07T02:24:41+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 20 (Oracle Corporation)"
 )
 @Component
